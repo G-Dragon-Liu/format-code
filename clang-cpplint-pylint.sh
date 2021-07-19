cc5e57a2203f1c1bb46f79c0721d5ff5e4efad7c
G-Dragon-Liu <56254082+G-Dragon-Liu@users.noreply.github.com>
2021-07-19 15:10:20 +0800

    Add files via upload

---
+++ clang-cpplint-pylint.sh
@@ -0,0 +1,57 @@
+
+prefix=/GitRep/mindspore/
+dataset_name=emnist
+
+#项目目录
+# prefix=/root/ori/phototour/mindspore/
+# #数据集的名字
+# dataset_name= emnist
+cd $prefix
+echo $PWD
+
+array=(mindspore/ccsrc/minddata/dataset/engine/datasetops/source/${dataset_name}_op.cc
+mindspore/ccsrc/minddata/dataset/engine/datasetops/source/${dataset_name}_op.h
+
+mindspore/ccsrc/minddata/dataset/engine/ir/datasetops/dataset_node.h
+mindspore/ccsrc/minddata/dataset/engine/ir/datasetops/source/${dataset_name}_node.cc
+mindspore/ccsrc/minddata/dataset/engine/ir/datasetops/source/${dataset_name}_node.h
+
+mindspore/ccsrc/minddata/dataset/api/datasets.cc
+mindspore/ccsrc/minddata/dataset/include/dataset/datasets.h
+mindspore/ccsrc/minddata/dataset/include/dataset/samplers.h
+
+mindspore/ccsrc/minddata/dataset/api/python/bindings/dataset/engine/ir/datasetops/source/bindings.cc
+
+
+tests/ut/cpp/dataset/c_api_dataset_${dataset_name}_test.cc
+tests/ut/cpp/dataset/${dataset_name}_op_test.cc)
+
+for file in ${array[@]}
+do
+    echo ${file}
+    clang-format -style=file -i  $prefix${file}
+done
+
+
+#bash -x scripts/check_clang_format.sh -l
+
+#cpplint
+
+for file in ${array[@]}
+do
+    echo ${file}
+    cpplint --repository=${prefix} --linelength=120 ${file}
+done
+
+
+pyfiles=(
+    mindspore/dataset/engine/validators.py
+    mindspore/dataset/engine/datasets.py
+    tests/ut/python/dataset/test_datasets_${dataset_name}.py
+)
+
+for file in ${pyfiles[@]}
+do
+    echo ${file}
+    pylint --rcfile=/format_code/pylintrc -j 4 --output-format=parseable ${file}
+done
